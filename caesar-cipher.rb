74dc1c144de9fb9b5f7df52741845177fd0588e1
EricMTodd <eterictodd@gmail.com>
2021-08-03 12:49:38 -0600

    Challenge complete.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -2,6 +2,7 @@ legend = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split(
 
 def encrypt(string, shift, legend)
   puts("Encrypting string: #{string}")
+  encryption = []
 
   string_array = string.split('')
 
@@ -9,13 +10,23 @@ def encrypt(string, shift, legend)
     if legend.include?(string_char)
       legend.each_with_index do |legend_char, k|
         if string_char == legend_char
-          puts("#{k}: #{legend_char}")
+          # puts("#{k}: #{legend_char}")
+          if k + shift > legend.length
+            puts("#{legend.length - k}: #{legend[legend.length - k]}")
+            encryption.push(legend[legend.length - k])
+          else
+            puts("#{k + shift}: #{legend[k + shift]}")
+            encryption.push(legend[k + shift])
+          end
         end
       end
     else
       puts("nil: #{string_char}")
+      encryption.push(string_char)
     end
   end
+  p(encryption.join(""))
+  return encryption.join("")
 end
 
-encrypt("Hello world!", 5, legend)
+encrypt("Hello world war Z0!", 5, legend)
