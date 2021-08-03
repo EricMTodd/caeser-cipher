2170f90d6a967431d6ea09ec943b0af77f1f8a00
EricMTodd <eterictodd@gmail.com>
2021-08-03 10:54:51 -0600

    Foundations laid.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,11 +1,21 @@
-alphanumerics = "abcdefghijklmnopqrstuvwxyz"
-chars_array = alphanumerics.split('')
+legend = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split('')
 
-p(chars_array)
-
-
-def encrypt(string, shift)
+def encrypt(string, shift, legend)
   puts("Encrypting string: #{string}")
+
+  string_array = string.split('')
+
+  string_array.each_with_index do |string_char, i|
+    if legend.include?(string_char)
+      legend.each_with_index do |legend_char, k|
+        if string_char == legend_char
+          puts("#{k}: #{legend_char}")
+        end
+      end
+    else
+      puts("nil: #{string_char}")
+    end
+  end
 end
 
-# encrypt("Hello world!", 0)
+encrypt("Hello world!", 5, legend)
