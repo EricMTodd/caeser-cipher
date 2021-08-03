5ff058c06480da374d173d416304d203172ad729
Eric M. Todd <eterictodd@gmail.com>
2021-08-02 20:56:29 -0600

    Putting in work!

---
+++ caesar-cipher.rb
@@ -0,0 +1,11 @@
+alphanumerics = "abcdefghijklmnopqrstuvwxyz"
+chars_array = alphanumerics.split('')
+
+p(chars_array)
+
+
+def encrypt(string, shift)
+  puts("Encrypting string: #{string}")
+end
+
+# encrypt("Hello world!", 0)
